822cf8240f10be6e4ffde176e6a7e2677dffb5ec
Roberto Marungo <robmarungo.dev@gmail.com>
2023-04-08 03:23:21 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+//Waits for the DOM to load first
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        buildToDo(e.target.new_task_description.value);
+        form.reset();
+    });
 });
+
+//Function that takes a Task
+function buildToDo(todo) {
+    let p = document.createElement("P");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", handleDelete);
+    p.textContent = `${todo} `;
+    btn.textContent = "X";
+    p.appendChild(btn);
+    document.querySelector("#list").appendChild(p);
+}
+
+//Function that deletes a Task
+function handleDelete(e) {
+    e.target.parentNode.remove();
+}
